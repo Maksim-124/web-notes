f7c7294d44c78ddba7f306b7f9574c9896632920
Maksim-124 <Bocman024@mail.ru>
2025-09-19 11:44:48 +0300

    +конспекты: Гриды и Микросетки

--- nav-bar.js
+++ nav-bar.js
@@ -1,19 +1,52 @@
 class Navbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+        <style>
+            nav {
+            display: flex;
+            justify-content: center;
+            
+        }
+
+            nav ul {
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                list-style: none;
+                margin: 10px;
+                padding: 0;
+            }
+
+            nav ul li {
+                margin: 0;
+                padding: 0;
+                }
+
+            .helpful_links ul {
+                display: grid;
+                grid-template-columns: 1fr 3fr;
+                column-gap: 10px;
+            }
+            .helpful_links li {
+                margin-bottom: 10px;
+        </style>
         <nav>
-            <a href="index.html">Конспект «Структура HTML-документа»</a>
-            <a href="mnemonical.html">Мнемонический словарь</a>
-            <a href="textmarkup.html">Разметка текста</a>
-            <a href="linksandimages.html">Ссылки и изображения</a>
-            <a href="css.html">Конспект: основы CSS</a>
-            <a href="text-formatting.html">Конспект «Оформление текста»</a>
-            <a href="selectors.html">Селекторы</a>
-            <a href="backgrounds.html">Фоны</a>
+            <ul>
+                <li><a href="index.html">Конспект «Структура HTML-документа»</a></li>
+                <li><a href="mnemonical.html">Мнемонический словарь</a></li>
+                <li><a href="textmarkup.html">Разметка текста</a></li>
+                <li><a href="linksandimages.html">Ссылки и изображения</a></li>
+                <li><a href="css.html">Конспект: основы CSS</a></li>
+                <li><a href="text-formatting.html">Конспект «Оформление текста»</a></li>
+                <li><a href="selectors.html">Селекторы</a></li>
+                <li><a href="backgrounds.html">Фоны</a></li>
+                <li><a href="block-model.html">Блочная модель</a></li>
+                <li><a href="grid.html">Гриды</a></li>
+                <li><a href="microgrids.html">Микро-сетки</a></li>
+            </ul>
         </nav>
         <aside class="helpful_links">
-            <ul>
-                <h4 style="margin: 0;">Полезные ссылки</h4>
+            <h4 style="margin: 0;">Полезные ссылки</h4>
+            <ul class="helpful_links-list">
                 <li><a href="https://www.w3.org/TR/CSS22/propidx.html" target="_blank" rel="noopener noreferrer" title="CSS стандарты">Стандарты CSS</a></li>
                 <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements" target="_blank" rel="noopener noreferrer" title="Справочник HTML тегов">Справочник HTML тегов</a></li>
                 <li><a href="https://github.com/yoksel/common-words" target="_blank" rel="noopener noreferrer" title="Слова часто используемые в CSS-классах">Common CSS words</a></li>
